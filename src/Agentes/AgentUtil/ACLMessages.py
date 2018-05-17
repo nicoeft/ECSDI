7a554c8fa1984db952b320f0ab8fa5d8750a7be9
Nico <nfrancisquelo2@gmail.com>
2018-05-17 16:07:22 +0200

    Un poco de todo

---
+++ src/Agentes/AgentUtil/ACLMessages.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+filename: ACLMessages
+
+Utilidades para tratar los mensajes FIPA ACL
+
+Created on 08/02/2014
+
+@author: javier
+"""
+__author__ = 'javier'
+
+from rdflib import Graph, Namespace
+import requests
+from rdflib.namespace import RDF, FOAF
+from AgentUtil.OntoNamespaces import ACL ,DSO
+from AgentUtil.Agent import Agent
+
+agn = Namespace("http://www.agentes.org#")
+
+def build_message(gmess, perf, sender=None, receiver=None,  content=None, msgcnt= 0):
+    """
+    Construye un mensaje como una performativa FIPA acl
+    Asume que en el grafo que se recibe esta ya el contenido y esta ligado al
+    URI en el parametro contenido
+
+    :param gmess: grafo RDF sobre el que se deja el mensaje
+    :param perf: performativa del mensaje
+    :param sender: URI del sender
+    :param receiver: URI del receiver
+    :param content: URI que liga el contenido del mensaje
+    :param msgcnt: numero de mensaje
+    :return:
+    """
+    # Añade los elementos del speech act al grafo del mensaje
+    mssid = 'message-'+str(sender.__hash__()) + '-{:{fill}4d}'.format(msgcnt, fill='0')
+    ms = ACL[mssid]
+    gmess.bind('acl', ACL)
+    gmess.add((ms, RDF.type, ACL.FipaAclMessage))
+    gmess.add((ms, ACL.performative, perf))
+    gmess.add((ms, ACL.sender, sender))
+    if receiver is not None:
+        gmess.add((ms, ACL.receiver, receiver))
+    if content is not None:
+        gmess.add((ms, ACL.content, content))
+    return gmess
+
+
+def send_message(gmess, address):
+    """
+    Envia un mensaje usando un request y retorna la respuesta como
+    un grafo RDF
+    """
+    msg = gmess.serialize(format='xml')
+    r = requests.get(address, params={'content': msg})
+
+    # Procesa la respuesta y la retorna como resultado como grafo
+    gr = Graph()
+    gr.parse(data=r.text)
+
+    return gr
+
+
+def get_message_properties(msg):
+    """
+    Extrae las propiedades de un mensaje ACL como un diccionario.
+    Del contenido solo saca el primer objeto al que apunta la propiedad
+
+    Los elementos que no estan, no aparecen en el diccionario
+    """
+    props = {'performative': ACL.performative, 'sender': ACL.sender,
+             'receiver': ACL.receiver, 'ontology': ACL.ontology,
+             'conversation-id': ACL['conversation-id'],
+             'in-reply-to': ACL['in-reply-to'], 'content': ACL.content}
+    msgdic = {} # Diccionario donde se guardan los elementos del mensaje
+
+    # Extraemos la parte del FipaAclMessage del mensaje
+    valid = msg.value(predicate=RDF.type, object=ACL.FipaAclMessage)
+
+    # Extraemos las propiedades del mensaje
+    if valid is not None:
+        for key in props:
+            val = msg.value(subject=valid, predicate=props[key])
+            if val is not None:
+                msgdic[key] = val
+    return msgdic
+
+def directory_search_agent(type, origen,agenteDirectorio, mss_cnt):
+    """
+    Busca en el servicio de registro mandando un
+    mensaje de request con una accion Search del servicio de directorio
+    
+    """
+
+    gmess = Graph()
+
+    gmess.bind('foaf', FOAF)
+    gmess.bind('dso', DSO)
+    reg_obj = agn[origen.name + '-search'] #nombre del graph -> AgenteCliente-search
+    gmess.add((reg_obj, RDF.type, DSO.Search))  #añadimos el tipo de RDF-> Serach
+    gmess.add((reg_obj, DSO.AgentType, type)) #añadimos el tipo de agente que estamos pidiendo es el parm de la funcion (DSO.AgenteMostrarProductos)
+
+    msg = build_message(gmess, perf=ACL.request,
+                        sender=origen.uri,
+                        receiver=agenteDirectorio.uri,
+                        content=reg_obj,
+                        msgcnt=mss_cnt)
+    gr = send_message(msg, agenteDirectorio.address)
+    mss_cnt += 1
+    # Obtenemos la direccion del agente de la respuesta
+    # No hacemos ninguna comprobacion sobre si es un mensaje valido
+    msg = gr.value(predicate=RDF.type, object=ACL.FipaAclMessage) #Obtenemos el mensaje fipa 
+    content = gr.value(subject=msg, predicate=ACL.content) #Obtenemos el contenido del mensaje fipa
+    ragn_addr = gr.value(subject=content, predicate=DSO.Address) #Obtenemos la adress del contenido
+    ragn_uri = gr.value(subject=content, predicate=DSO.Uri) #Obtenemos la uri del contenido
+    name = gr.value(subject=content, predicate=FOAF.name) #Obtenemos el nombre del agente del contenido
+    return Agent(name,ragn_uri,ragn_addr,None)
---
+++ src/Agentes/AgenteCliente.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+"""
+filename: AgenteCliente
+
+Antes de ejecutar hay que añadir la raiz del proyecto a la variable PYTHONPATH
+
+Agente que interacciona con el usuario
+
+
+Created on 09/02/2014
+
+@author: amazon2
+"""
+
+from __future__ import print_function
+from multiprocessing import Process
+import socket
+import argparse
+
+from flask import Flask, render_template, request
+from rdflib import Graph, Namespace, RDF, URIRef, Literal, XSD
+from rdflib.namespace import FOAF, RDF
+import requests
+
+from AgentUtil.OntoNamespaces import ACL, DSO, AM2
+from AgentUtil.FlaskServer import shutdown_server
+from AgentUtil.ACLMessages import build_message, send_message, directory_search_agent
+from AgentUtil.Agent import Agent
+from AgentUtil.Logging import config_logger
+
+__author__ = 'amazon2'
+
+# Definimos los parametros de la linea de comandos
+parser = argparse.ArgumentParser()
+parser.add_argument('--open', help="Define si el servidor est abierto al exterior o no", action='store_true',
+                    default=False)
+parser.add_argument('--port', type=int, help="Puerto de comunicacion del agente")
+parser.add_argument('--dhost', default='localhost', help="Host del agente de directorio")
+parser.add_argument('--dport', type=int, help="Puerto de comunicacion del agente de directorio")
+
+# Logging
+logger = config_logger(level=1)
+
+# parsing de los parametros de la linea de comandos
+args = parser.parse_args()
+
+# Configuration stuff
+if args.port is None:
+    port = 9010
+else:
+    port = args.port
+
+if args.open is None:
+    hostname = '0.0.0.0'
+else:
+    hostname = "localhost"
+    # hostname = socket.gethostname()
+
+if args.dport is None:
+    dport = 9000
+else:
+    dport = args.dport
+
+if args.dhost is None:
+    dhostname = socket.gethostname()
+else:
+    dhostname = args.dhost
+
+# Flask stuff
+app = Flask(__name__)
+
+# Configuration constants and variables
+agn = Namespace("http://www.agentes.org#")
+
+# Contador de mensajes
+mss_cnt = 0
+
+# Datos del Agente
+AgenteCliente = Agent('AgenteCliente',
+                       agn.AgenteCliente,
+                       'http://%s:%d/comm' % (hostname, port),
+                       'http://%s:%d/Stop' % (hostname, port))
+
+# Directory agent address
+DirectoryAgent = Agent('DirectoryAgent',
+                       agn.Directory,
+                       'http://%s:%d/Register' % (dhostname, dport),
+                       'http://%s:%d/Stop' % (dhostname, dport))
+
+# Global dsgraph triplestore
+dsgraph = Graph()
+
+
+def infoagent_search_message(addr, ragn_uri):
+    """
+    Envia una accion a un agente de informacion
+    """
+    global mss_cnt
+    logger.info('Hacemos una peticion a AgenteMostrarProductos')
+
+    gmess = Graph()
+
+   
+    
+    gmess.bind('foaf', FOAF)
+    gmess.bind('am2', AM2)
+     # Creamos el sujeto -> contenido del mensaje
+    sj_contenido = agn[AgenteCliente.name + 'Peticion_productos_disponibles' + str(mss_cnt)]
+    #le damos un tipo
+    gmess.add((sj_contenido, RDF.type, AM2.Peticion_productos_disponibles))
+    
+    # Añadimos restriccion modelo
+    sj_modelo = AM2['Modelo' + str(mss_cnt)] #creamos una instancia con nombre Modelo1
+    gmess.add((sj_modelo, RDF.type, AM2.Modelo)) # indicamos que es de tipo Modelo
+    gmess.add((sj_modelo, AM2.tieneModelo, Literal('E1234H'))) #le damos valor a su data property
+    
+    #añadimos el modelo al conenido con su object property
+    gmess.add((sj_contenido, AM2.Restricciones_clientes, URIRef(sj_contenido))) 
+    msg = build_message(gmess, perf=ACL.request,
+                        sender=AgenteCliente.uri,
+                        receiver=ragn_uri,
+                        msgcnt=mss_cnt)
+    gr = send_message(msg, addr)
+    mss_cnt += 1
+    logger.info('Recibimos respuesta a la peticion al servicio de informacion')
+
+    return gr
+
+
+@app.route("/iface", methods=['GET', 'POST'])
+def browser_iface():
+    """
+    Permite la comunicacion con el agente via un navegador
+    via un formulario
+    """
+    if request.method == 'GET':
+        return render_template('iface.html')
+    else:
+        user = request.form['username']
+        mess = request.form['message']
+        return render_template('riface.html', user=user, mess=mess)
+
+
+@app.route("/Stop")
+def stop():
+    """
+    Entrypoint que para el agente
+
+    :return:
+    """
+    tidyup()
+    shutdown_server()
+    return "Parando Servidor"
+
+
+@app.route("/comm")
+def comunicacion():
+    """
+    Entrypoint de comunicacion del agente
+    """
+    return "Hola"
+
+
+def tidyup():
+    """
+    Acciones previas a parar el agente
+
+    """
+    pass
+
+
+def agentbehavior1():
+    """
+    Un comportamiento del agente
+
+    :return:
+    """
+
+    # Buscamos en el directorio
+    # un agente de mostrar productos
+    logger.info('Buscamos en el servicio de registro')
+    AgenteMostrarProductos = directory_search_agent(DSO.AgenteMostrarProductos,AgenteCliente,DirectoryAgent,mss_cnt)
+    logger.info('Recibimos informacion del agente')
+
+
+    # Ahora mandamos un objeto de tipo request mandando una accion de tipo Search
+    # que esta en una supuesta ontologia de acciones de agentes
+    infoagent_search_message(AgenteMostrarProductos.address,AgenteMostrarProductos.uri)
+    infoagent_search_message(AgenteMostrarProductos.address,AgenteMostrarProductos.uri)
+    infoagent_search_message(AgenteMostrarProductos.address,AgenteMostrarProductos.uri)
+    
+    # r = requests.get(ra_stop)
+    # print r.text
+
+    # Selfdestruct
+    requests.get(AgenteCliente.stop)
+
+
+if __name__ == '__main__':
+    # Ponemos en marcha los behaviors
+    ab1 = Process(target=agentbehavior1)
+    ab1.start()
+
+    # Ponemos en marcha el servidor
+    app.run(host=hostname, port=port)
+
+    # Esperamos a que acaben los behaviors
+    ab1.join()
+    logger.info('The End')
