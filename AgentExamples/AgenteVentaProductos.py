e7e786f1bd370abf1fb837c0cd22a34100ebe28d
Alex Huang Feng <alexhuang05@gmail.com>
2018-05-17 13:36:51 +0200

    base agente Venta productos

---
+++ AgentExamples/AgenteVentaProductos.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 15:58:13 2013
+
+Esqueleto de agente usando los servicios web de Flask
+
+/comm es la entrada para la recepcion de mensajes del agente
+/Stop es la entrada que para el agente
+
+Tiene una funcion AgentBehavior1 que se lanza como un thread concurrente
+
+Asume que el agente de registro esta en el puerto 9000
+
+@author: javier
+"""
+
+from __future__ import print_function
+from multiprocessing import Process, Queue
+import socket
+import argparse
+
+from rdflib import Namespace, Graph
+from flask import Flask
+
+from AgentUtil.FlaskServer import shutdown_server
+from AgentUtil.Agent import Agent
+from AgentUtil.Logging import config_logger
+
+__author__ = 'javier'
+
+# Definimos los parametros de la linea de comandos
+parser = argparse.ArgumentParser()
+parser.add_argument('--open', help="Define si el servidor esta abierto al exterior o no", action='store_true',
+                    default=False)
+parser.add_argument('--port', type=int, help="Puerto de comunicacion del agente")
+parser.add_argument('--dhost', default='localhost', help="Host del agente de directorio")
+parser.add_argument('--dport', type=int, help="Puerto de comunicacion del agente de directorio")
+
+# Logging
+logger = config_logger(level=1)
+
+# Configuration stuff
+hostname = socket.gethostname()
+port = 9010
+
+# parsing de los parametros de la linea de comandos
+args = parser.parse_args()
+
+# Configuration stuff
+if args.port is None:
+    port = 9010
+else:
+    port = args.port
+
+if args.open is None:
+    hostname = '0.0.0.0'
+else:
+    hostname = "localhost"
+    # hostname = socket.gethostname()
+
+if args.dport is None:
+    dport = 9000
+else:
+    dport = args.dport
+
+    # dhostname = socket.gethostname()
+if args.dhost is None:
+    dhostname = "localhost"
+else:
+    dhostname = args.dhost
+
+
+agn = Namespace("http://www.agentes.org#")
+
+# Contador de mensajes
+mss_cnt = 0
+
+# Datos del Agente
+
+AgenteVentaProductos = Agent('AgenteVentaProductos',
+                       agn.AgenteVentaProductos,
+                       'http://%s:%d/comm' % (hostname, port),
+                       'http://%s:%d/Stop' % (hostname, port))
+
+# Directory agent address
+DirectoryAgent = Agent('DirectoryAgent',
+                       agn.Directory,
+                       'http://%s:9000/Register' % hostname,
+                       'http://%s:9000/Stop' % hostname)
+
+
+# Global triplestore graph
+dsgraph = Graph()
+
+cola1 = Queue()
+
+# Flask stuff
+app = Flask(__name__)
+
+
+@app.route("/comm")
+def comunicacion():
+    """
+    Entrypoint de comunicacion
+    """
+    global dsgraph
+    global mss_cnt
+    pass
+
+
+@app.route("/Stop")
+def stop():
+    """
+    Entrypoint que para el agente
+
+    :return:
+    """
+    tidyup()
+    shutdown_server()
+    return "Parando Servidor"
+
+
+def tidyup():
+    """
+    Acciones previas a parar el agente
+
+    """
+    pass
+
+
+def agentbehavior1(cola):
+    """
+    Un comportamiento del agente
+
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    # Ponemos en marcha los behaviors
+    ab1 = Process(target=agentbehavior1, args=(cola1,))
+    ab1.start()
+
+    # Ponemos en marcha el servidor
+    app.run(host=hostname, port=port)
+
+    # Esperamos a que acaben los behaviors
+    ab1.join()
+    print('The End')
+
+
--- AgentExamples/PUERTOS.md
+++ AgentExamples/PUERTOS.md
@@ -2,3 +2,4 @@
 - 9000 SimpleDirectoryService
 - 9010 AgenteCliente
 - 9011 AgenteMostrarProductos
+- 9012 AgenteVentaProductos
